f6210ef60c7d57fe24240c1bdafcafeca1531f2e
okadak <okadak@sci.hokudai.ac.jp>
2024-11-22 09:25:38 +0900

    Update environment configuration and data reader implementation
    - Modified env.yml to update package versions for compatibility.
    - Enhanced data_reader.py with additional functionality and optimizations.

--- phasenet/data_reader.py
+++ phasenet/data_reader.py
@@ -431,7 +431,7 @@ class DataReader:
 
             station_ids = list(set(station_ids))
             if len(station_ids) > 1:
-                print(f"{station_ids = }")
+                print(f"station_ids = {station_ids}")
                 raise
             assert (len(station_ids) == 1, f"Error: {fname} has multiple stations {station_ids}")
 
